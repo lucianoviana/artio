25cdbf0239411f2ad0353d8a6faad05da21c0885
Luciano Viana <luciano@weareadaptive.com>
2024-10-09 15:50:30 +0200

    making GatewayToGatewaySystemTest deterministic when testing resendRequests sent after burst of exec reports

--- artio-system-tests/src/test/java/uk/co/real_logic/artio/system_tests/GatewayToGatewaySystemTest.java
+++ artio-system-tests/src/test/java/uk/co/real_logic/artio/system_tests/GatewayToGatewaySystemTest.java
@@ -85,6 +85,7 @@ public class GatewayToGatewaySystemTest extends AbstractGatewayToGatewaySystemTe
     public static final boolean METADATA_OFF = false;
 
     private boolean testWithMetaData = METADATA_OFF;
+    private int orderId;
 
     @Before
     public void launch()
@@ -354,7 +355,7 @@ public class GatewayToGatewaySystemTest extends AbstractGatewayToGatewaySystemTe
             return count >= 1;
         });
 
-        testSystem.await(() -> fakeResendRequestController.callCount() == 2);
+        assertEquals(2, fakeResendRequestController.callCount());
     }
 
     // Test exists to replicate a faily complex bug involving a sequence number issue after a library timeout.
@@ -1329,16 +1330,20 @@ public class GatewayToGatewaySystemTest extends AbstractGatewayToGatewaySystemTe
 
     private void exchangeExecutionReport(final Session session, final FakeOtfAcceptor otfAcceptor)
     {
+        orderId++;
+        final String newOrderId = "order" + orderId;
         final ExecutionReportEncoder executionReport = new ExecutionReportEncoder();
         executionReport
-            .orderID("order")
+            .orderID(newOrderId)
             .execID("exec")
             .execType(ExecType.FILL)
             .ordStatus(OrdStatus.FILLED)
             .side(Side.BUY);
         executionReport.instrument().symbol("IBM");
         testSystem.awaitSend(() -> session.trySend(executionReport));
-        testSystem.awaitMessageOf(otfAcceptor, EXECUTION_REPORT_MESSAGE_AS_STR);
+        testSystem.awaitMessageOf(otfAcceptor,
+            EXECUTION_REPORT_MESSAGE_AS_STR,
+            msg -> msg.get(ORDER_ID).equals(newOrderId));
     }
 
     private void reacquireSession(
