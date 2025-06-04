lucianoviana/artio

commit 605785b3484a62653b9c1aef946e4d4d6d91ecb9

Adding validation for char/boolean tag value length (#546)

artio-codecs/src/test/java/uk/co/real_logic/artio/dictionary/generation/AbstractDecoderGeneratorTest.java
@@ -1406,6 +1406,56 @@ public abstract class AbstractDecoderGeneratorTest
     }
 
     @Test
+    public void decodesInvalidSizeBooleanValueWithNoValidation() throws Exception
+    {
+        final Decoder decoder = decodeHeartbeatWithoutValidation(INCORRECT_BOOLEAN_VALUE_MESSAGE);
+        assertEquals(true, getBooleanField(decoder));
+        assertArrayEquals(new char[]{'Y', 'Y'}, getChars(decoder, "booleanFieldAsChars"));
+
+        assertValid(decoder);
+    }
+
+    @Test
+    public void decodesInvalidSizeBooleanValue() throws Exception
+    {
+        final Decoder decoder = decodeHeartbeat(INCORRECT_BOOLEAN_VALUE_MESSAGE);
+        assertEquals(true, getBooleanField(decoder));
+        assertArrayEquals(new char[]{'Y', 'Y'}, getChars(decoder, "booleanFieldAsChars"));
+
+        assertInvalid(decoder, 5, 118);
+    }
+
+    @Test
+    public void decodesInvalidSizeNoEnumCharValue() throws Exception
+    {
+        final Decoder decoder = decodeHeartbeat(INCORRECT_SIZE_NO_ENUM_CHAR_VALUE_MESSAGE);
+        assertEquals('a', getChar(decoder, "charNoEnumField"));
+        assertArrayEquals(new char[]{'a', 'b'}, getChars(decoder, "charNoEnumFieldAsChars"));
+
+        assertInvalid(decoder, 5, 144);
+    }
+
+    @Test
+    public void decodesInvalidSizeCharValue() throws Exception
+    {
+        final Decoder decoder = decodeHeartbeat(INCORRECT_SIZE_CHAR_VALUE_MESSAGE);
+        assertEquals('a', getCharField(decoder));
+        assertArrayEquals(new char[]{'a', 'b'}, getChars(decoder, "charFieldAsChars"));
+
+        assertInvalid(decoder, 5, 128);
+    }
+
+    @Test
+    public void decodesInvalidCharValue() throws Exception
+    {
+        final Decoder decoder = decodeHeartbeat(INCORRECT_SIZE_2_CHAR_VALUE_MESSAGE);
+        assertEquals('z', getCharField(decoder));
+        assertArrayEquals(new char[]{'z'}, getChars(decoder, "charFieldAsChars"));
+
+        assertInvalid(decoder, 5, 128);
+    }
+
+    @Test
     public void doesNotValidateIfNoEnumValuesPresent() throws Exception
     {
         final Decoder decoder = decodeHeartbeat(MULTI_CHAR_VALUE_NO_ENUM_MESSAGE);
@@ -1685,6 +1735,11 @@ public abstract class AbstractDecoderGeneratorTest
     private char[] getMultiCharField(final Decoder decoder) throws Exception
     {
         return getChars(decoder, "multiCharField");
+    }
+
+    private char getCharField(final Decoder decoder) throws Exception
+    {
+        return getChar(decoder, "charField");
     }
 
     private char[] getMultiCharNoEnumField(final Decoder decoder) throws Exception

artio-codecs/src/main/java/uk/co/real_logic/artio/dictionary/generation/DecoderGenerator.java
@@ -542,6 +542,12 @@ class DecoderGenerator extends Generator
             .map(this::generateEnumValidation)
             .collect(joining("\n"));
 
+        final String charSizeValidation = aggregate
+            .allFieldsIncludingComponents()
+            .filter((entry) -> entry.element().isCharOrBooleanBasedField())
+            .map(this::generateCharSizeValidation)
+            .collect(joining("\n"));
+
         //maybe this should look at groups on components too?
         final String groupValidation = aggregate
             .allGroupsIncludingComponents()
@@ -574,7 +580,7 @@ class DecoderGenerator extends Generator
 
         out.append(String.format(
             (isGroup ? generateAllGroupFields(aggregate) :
-            "    private final IntHashSet alreadyVisitedFields = new IntHashSet(%5$d);\n\n" +
+            "    private final IntHashSet alreadyVisitedFields = new IntHashSet(%6$d);\n\n" +
             "    private final IntHashSet unknownFields = new IntHashSet(10);\n\n") +
             "    private final IntHashSet missingRequiredFields = new IntHashSet(%1$d);\n\n" +
             "    public boolean validate()\n" +
@@ -595,11 +601,13 @@ class DecoderGenerator extends Generator
             "        }\n" +
             "%3$s" +
             "%4$s" +
+            "%5$s" +
             "        return true;\n" +
             "    }\n\n",
             sizeHashSet(requiredFields),
             messageValidation,
             enumValidation,
+            charSizeValidation,
             groupValidation,
             2 * aggregate.allFieldsIncludingComponents().count()));
     }
@@ -653,6 +661,33 @@ class DecoderGenerator extends Generator
             prefix,
             name,
             constantName(field.name()));
+    }
+
+    private CharSequence generateCharSizeValidation(final Entry entry)
+    {
+        final Field field = (Field)entry.element();
+        if (!field.isCharOrBooleanBasedField())
+        {
+            return "";
+        }
+
+        final String name = entry.name();
+        final int tagNumber = field.number();
+        final Type type = field.type();
+        final String propertyName = formatPropertyName(name);
+
+        final String charsValidation = addCharSizeValidation(type, propertyName, tagNumber);
+
+        return
+          entry.required() ? charsValidation :
+            String.format(
+              "        if (has%1$s)\n" +
+                "        {\n" +
+                "%2$s" +
+                "        }\n",
+              entry.name(),
+              charsValidation
+            );
     }
 
     private CharSequence generateEnumValidation(final Entry entry)
@@ -724,6 +759,30 @@ class DecoderGenerator extends Generator
                 entry.name(),
                 enumValidationMethod
             );
+    }
+
+    private static String addCharSizeValidation(
+        final Type type,
+        final String propertyName,
+        final int tagNumber)
+    {
+        String charsValidation = "";
+        if (type.isCharOrBooleanBased())
+        {
+
+            charsValidation = String.format(
+                "        if (%3$s && %1$sAsChars.length > 1)\n" +
+                "        {\n" +
+                "            invalidTagId = %2$s;\n" +
+                "            rejectReason = " + VALUE_IS_INCORRECT + ";\n" +
+                "            return false;\n" +
+                "        }\n",
+              propertyName,
+              tagNumber,
+              CODEC_VALIDATION_ENABLED);
+
+        }
+        return charsValidation;
     }
 
     private CharSequence generateGroupValidation(final Entry entry)
@@ -1400,6 +1459,11 @@ class DecoderGenerator extends Generator
 
         return String.format(
             "    %10$s %1$s %2$s%3$s;\n\n" +
+              (type.isCharOrBooleanBased() ? "    %10$s char[] %2$sAsChars = new char[1];\n" +
+                "    public char[] %2$sAsChars()" +
+                "    {\n" +
+                "       return %2$sAsChars;\n" +
+                "    }\n" : "") +
             "%4$s" +
             "    %11$spublic %1$s %2$s()\n" +
             "    {\n" +
@@ -2003,10 +2067,12 @@ class DecoderGenerator extends Generator
             "%s" +
             "%s" +
             "%s" +
+            "%s" +
             "                break;\n",
             constantName(name),
             optionalAssign(entry),
             fieldDecodeMethod(field, fieldName),
+            readCharsFromBuffer(field, fieldName),
             storeOffsetForVariableLengthFields(field.type(), fieldName),
             storeLengthForVariableLengthFields(field.type(), fieldName),
             suffix);
@@ -2126,6 +2192,17 @@ class DecoderGenerator extends Generator
                 throw new UnsupportedOperationException("Unknown type: " + field.type() + " in " + fieldName);
         }
         return prefix + decodeMethod + ";\n";
+    }
+
+    private String readCharsFromBuffer(final Field field, final String fieldName)
+    {
+        if (!field.type().isCharOrBooleanBased())
+        {
+            return "";
+        }
+
+        return String.format("                %1$sAsChars = buffer.getChars(%1$sAsChars, valueOffset, " +
+          "valueLength);\n", fieldName);
     }
 
     protected String stringAppendTo(final String fieldName)

artio-codecs/src/main/java/uk/co/real_logic/artio/dictionary/ir/Entry.java
@@ -219,6 +219,11 @@ public final class Entry
             return this instanceof Field && ((Field)this).isEnum();
         }
 
+        default boolean isCharOrBooleanBasedField()
+        {
+            return this instanceof Field && ((Field)this).isCharOrBooleanBasedField();
+        }
+
         String name();
     }
 }

artio-codecs/src/main/java/uk/co/real_logic/artio/dictionary/ir/Field.java
@@ -103,6 +103,12 @@ public final class Field implements Element
         return !values.isEmpty();
     }
 
+    @Override
+    public boolean isCharOrBooleanBasedField()
+    {
+        return type.isCharBased || type.isBooleanBased;
+    }
+
     public boolean hasSharedSometimesEnumClash()
     {
         return hasSharedSometimesEnumClash;
@@ -180,7 +186,7 @@ public final class Field implements Element
         XMLDATA(false, false, false, false, false),
 
         // Boolean types
-        BOOLEAN(false, false, false, false, false),
+        BOOLEAN(false, false, false, false, false, true),
 
         UTCTIMESTAMP(true, false, false, false, false), // YYYYMMDD-HH:MM:SS or YYYYMMDD-HH:MM:SS.sss
         UTCTIMEONLY(true, false, false, false, false), // HH:MM:SS or HH:MM:SS.sss
@@ -195,6 +201,24 @@ public final class Field implements Element
         private final boolean isFloatBased;
         private final boolean isMultiValue;
         private final boolean isCharBased;
+        private final boolean isBooleanBased;
+
+        Type(
+            final boolean isStringBased,
+            final boolean isIntBased,
+            final boolean isFloatBased,
+            final boolean isMultiValue,
+            final boolean isCharBased,
+            final boolean isBooleanBased
+        )
+        {
+            this.isStringBased = isStringBased;
+            this.isIntBased = isIntBased;
+            this.isFloatBased = isFloatBased;
+            this.isMultiValue = isMultiValue;
+            this.isCharBased = isCharBased;
+            this.isBooleanBased = isBooleanBased;
+        }
 
         Type(
             final boolean isStringBased,
@@ -204,11 +228,7 @@ public final class Field implements Element
             final boolean isCharBased
         )
         {
-            this.isStringBased = isStringBased;
-            this.isIntBased = isIntBased;
-            this.isFloatBased = isFloatBased;
-            this.isMultiValue = isMultiValue;
-            this.isCharBased = isCharBased;
+            this(isStringBased, isIntBased, isFloatBased, isMultiValue, isCharBased, false);
         }
 
         public boolean isStringBased()
@@ -229,6 +249,11 @@ public final class Field implements Element
         public boolean isCharBased()
         {
             return isCharBased;
+        }
+
+        public boolean isCharOrBooleanBased()
+        {
+            return isCharBased || isBooleanBased;
         }
 
         public boolean isDataBased()

artio-codecs/src/test/java/uk/co/real_logic/artio/dictionary/ExampleDictionary.java
@@ -355,6 +355,22 @@ public final class ExampleDictionary
         "8=FIX.4.4\0019=61\00135=0\001115=abc\001116=2\001117=1.1" +
         "\001132=a b\001127=19700101-00:00:00.001\00110=225\001";
 
+    public static final String INCORRECT_BOOLEAN_VALUE_MESSAGE =
+        "8=FIX.4.4\0019=61\00135=0\001115=abc\001116=2\001117=1.1" +
+        "\001118=YY\001127=19700101-00:00:00.001\00110=225\001";
+
+    public static final String INCORRECT_SIZE_NO_ENUM_CHAR_VALUE_MESSAGE =
+        "8=FIX.4.4\0019=61\00135=0\001115=abc\001116=2\001117=1.1" +
+        "\001144=ab\001127=19700101-00:00:00.001\00110=225\001";
+
+    public static final String INCORRECT_SIZE_CHAR_VALUE_MESSAGE =
+        "8=FIX.4.4\0019=61\00135=0\001115=abc\001116=2\001117=1.1" +
+        "\001128=ab\001127=19700101-00:00:00.001\00110=225\001";
+
+    public static final String INCORRECT_SIZE_2_CHAR_VALUE_MESSAGE =
+        "8=FIX.4.4\0019=61\00135=0\001115=abc\001116=2\001117=1.1" +
+        "\001128=z\001127=19700101-00:00:00.001\00110=225\001";
+
     public static final String MULTI_CHAR_VALUE_NO_ENUM_MESSAGE =
         "8=FIX.4.4\0019=65\00135=0\001115=abc\001116=2\001117=1.1" +
         "\001134=a b z f\001127=19700101-00:00:00.001\00110=007\001";
@@ -553,6 +569,7 @@ public final class ExampleDictionary
         final Field booleanField = registerField(messageEgFields, 118, "BooleanField", Type.BOOLEAN);
         final Field dataField = registerField(messageEgFields, 119, "DataField", Type.DATA);
         final Field someTime = registerField(messageEgFields, 127, "SomeTimeField", Type.UTCTIMESTAMP);
+        final Field charNoEnumField = registerField(messageEgFields, 144, "CharNoEnumField", Type.CHAR);
         final Field charField = registerField(messageEgFields, 128, "CharField", Type.CHAR)
             .addValue("a", "One")
             .addValue("b", "Two");
@@ -622,6 +639,7 @@ public final class ExampleDictionary
         heartbeat.optionalEntry(dataFieldLength);
         heartbeat.optionalEntry(dataField);
         heartbeat.optionalEntry(charField);
+        heartbeat.optionalEntry(charNoEnumField);
         heartbeat.optionalEntry(multiCharField);
         heartbeat.optionalEntry(multiValStringField);
         heartbeat.optionalEntry(multiStringValField);

commit 4c6906da7c3daa2eddf46ef9ab6f5d90bcedf210

adding a test to ensure bug introduced by #546 does not occur after reset then new decoding

artio-codecs/src/test/java/uk/co/real_logic/artio/dictionary/generation/AbstractDecoderGeneratorTest.java
@@ -1416,6 +1416,24 @@ public abstract class AbstractDecoderGeneratorTest
     }
 
     @Test
+    public void shouldNotRetainInvalidBooleanCharValueSizeAfterNewMessageParsed() throws Exception
+    {
+        final Decoder decoder = decodeHeartbeat(INCORRECT_BOOLEAN_VALUE_MESSAGE);
+        assertEquals(true, getBooleanField(decoder));
+        assertEquals(2, getInt(decoder, "booleanFieldLength"));
+
+        assertInvalid(decoder, 5, 118);
+
+        decoder.reset();
+
+        decode(ENCODED_MESSAGE, decoder);
+        assertEquals(true, getBooleanField(decoder));
+        assertEquals(1, getInt(decoder, "booleanFieldLength"));
+        assertValid(decoder);
+
+    }
+
+    @Test
     public void decodesInvalidSizeBooleanValue() throws Exception
     {
         final Decoder decoder = decodeHeartbeat(INCORRECT_BOOLEAN_VALUE_MESSAGE);